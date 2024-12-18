182fab12fc76213bf3461b7dd9c6691d8cc51eba
shlavik <shlavik@gmail.com>
2024-12-19 03:50:16 +0400

    0.0.4: Node sale tablet

--- src/lib/node-sale/Card.svelte
+++ src/lib/node-sale/Card.svelte
@@ -13,10 +13,10 @@
 </script>
 
 <div
-	class={`flex h-[400px] flex-col justify-between rounded-[20px] px-11 py-[52px] mobile:h-[260px] mobile:p-9 mobile:pt-11 ${isNormal ? 'bg-white/5' : 'bg-accent'}`}
+	class={`flex h-[400px] flex-col justify-between rounded-[20px] px-11 py-[52px] tablet:h-[260px] tablet:rounded-[24px] tablet:px-8 tablet:pb-9 tablet:pt-11 mobile:h-[260px] mobile:p-9 mobile:pt-11 ${isNormal ? 'bg-white/5' : 'bg-accent'}`}
 >
 	<span
-		class={`text-[99px] font-medium leading-[0.74] text-darkness mobile:text-[64px] ${isNormal ? 'text-light' : 'text-darkness'}`}
+		class={`text-[99px] font-medium leading-[0.74] text-darkness tablet:text-[68px] mobile:text-[64px] ${isNormal ? 'text-light' : 'text-darkness'}`}
 	>
 		{value}
 	</span>
