a0a80aedf91967ff166b8f983c3feb918f63a3e0
shlavik <shlavik@gmail.com>
2024-12-19 05:03:29 +0400

    0.0.5: Node sale desktop

--- src/lib/node-sale/Card.svelte
+++ src/lib/node-sale/Card.svelte
@@ -13,15 +13,15 @@
 </script>
 
 <div
-	class={`flex h-[400px] flex-col justify-between rounded-[20px] px-11 py-[52px] tablet:h-[260px] tablet:rounded-[24px] tablet:px-8 tablet:pb-9 tablet:pt-11 mobile:h-[260px] mobile:p-9 mobile:pt-11 ${isNormal ? 'bg-white/5' : 'bg-accent'}`}
+	class={`flex h-[400px] flex-col justify-between rounded-[20px] px-11 py-[52px] desktop:h-80 desktop:px-8 desktop:pb-9 desktop:pt-11 tablet:h-[260px] tablet:rounded-[24px] tablet:px-8 tablet:pb-9 tablet:pt-11 mobile:h-[260px] mobile:p-9 mobile:pt-11 ${isNormal ? 'bg-white/5' : 'bg-accent'}`}
 >
 	<span
-		class={`text-[99px] font-medium leading-[0.74] text-darkness tablet:text-[68px] mobile:text-[64px] ${isNormal ? 'text-light' : 'text-darkness'}`}
+		class={`text-[99px] font-medium leading-[0.74] text-darkness desktop:text-[68px] tablet:text-[68px] mobile:text-[64px] ${isNormal ? 'text-light' : 'text-darkness'}`}
 	>
 		{value}
 	</span>
 	<span
-		class={`font-secondary text-[17px] font-light leading-[1.55] mobile:text-[14px] ${isNormal ? 'text-white/60' : 'text-dark'}`}
+		class={`font-secondary text-[17px] font-light leading-[1.55] desktop:text-sm mobile:text-[14px] ${isNormal ? 'text-white/60' : 'text-dark'}`}
 	>
 		{description}
 	</span>
--- src/routes/node-sale/NodeSale.svelte
+++ src/routes/node-sale/NodeSale.svelte
@@ -6,21 +6,21 @@
 	class="grid grid-cols-2 gap-6 rounded-[40px] bg-sale p-5 tablet:gap-3 tablet:rounded-[36px] tablet:p-3 mobile:grid-cols-1"
 >
 	<div
-		class="flex flex-col gap-4 pb-[66px] pl-[148px] pt-20 tablet:pl-10 tablet:pt-12 mobile:px-3 mobile:pb-2 mobile:pt-10"
+		class="flex flex-col gap-4 pb-[66px] pl-[148px] pt-20 desktop:pb-14 desktop:pl-[68px] desktop:pt-[60px] tablet:pl-10 tablet:pt-12 mobile:px-3 mobile:pb-2 mobile:pt-10"
 	>
 		<span class="text-sm leading-[1.1] text-white/60">Our DePIN</span>
 		<h3
-			class="flex flex-col text-5xl font-medium leading-[1.2] tablet:text-[32px] mobile:text-[32px]"
+			class="flex flex-col text-5xl font-medium leading-[1.2] desktop:text-[32px] tablet:text-[32px] mobile:text-[32px]"
 		>
 			<span>The largest DePIN</span>
 			<span>on the market</span>
 		</h3>
 	</div>
 	<div
-		class="flex flex-col justify-end pb-[66px] pr-[64px] tablet:pr-8 mobile:px-3 mobile:pb-6 mobile:pt-2"
+		class="flex flex-col justify-end pb-[66px] pr-[64px] desktop:pb-14 tablet:pr-8 mobile:px-3 mobile:pb-6 mobile:pt-2"
 	>
 		<p
-			class="flex-col font-secondary text-[19px] font-light leading-[1.55] text-white/60 lg-desktop:flex sm-desktop:flex tablet:block tablet:text-[15px] mobile:text-[15px]"
+			class="flex-col font-secondary text-[19px] font-light leading-[1.55] text-white/60 lg-desktop:flex md-desktop:block desktop:flex desktop:text-[15px] sm-desktop:block tablet:text-[15px] mobile:text-[15px]"
 		>
 			<span>
 				Our decentralized infrastructure network gets stronger with each user.
